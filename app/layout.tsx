2ae22f77c6803cfdd5a6a6ef90a79db41b6f701b
phuoc <phuoc.anonydev2k3@gmail.com>
2024-10-21 14:49:11 +0700

    deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
 import { MainLayout } from '@/components/layouts/MainLayout';
 import { Catamaran } from 'next/font/google';
+import { Suspense } from 'react';
 export const metadata: Metadata = {
   title: 'Teletrack',
   description: 'Teletrack',
@@ -17,7 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`antialiased ${catamaranFont.className}`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          <MainLayout>{children}</MainLayout>
+          <MainLayout>
+            <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+          </MainLayout>
         </ThemeProvider>
       </body>
     </html>
